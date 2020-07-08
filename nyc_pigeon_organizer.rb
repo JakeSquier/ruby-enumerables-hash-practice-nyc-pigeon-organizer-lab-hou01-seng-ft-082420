f7215bec8cfe5462964d4aaba7c904480eb6e478
Jacob Squier <jacob.squier@gmail.com>
2020-07-08 23:33:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, name|
-      names.each
+    value.each do |inner_key, names|
+      names.each do |name| 
+        
 end
